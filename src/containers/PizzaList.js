51f182a5294e24470675aceb1ae3dd34ffee489c
dawncronin <github email address>
2020-01-13 16:05:11 -0800

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -14,9 +14,7 @@ class PizzaList extends Component {
           </tr>
         </thead>
         <tbody>
-          {
-            //render Pizza here
-          }
+          {this.props.pizzas.map(pizza => < Pizza pizza={pizza} key={pizza.id} editPizza={this.props.editPizza}/>)          }
         </tbody>
       </table>
     );
